8b599020d54949ac36863429a3c743c165fc5e99
mohamed <mohamed.boumansour.ext@francetv.fr>
2016-09-21 22:03:45 +0200

    Add ressources file

---
+++ MeetXSweat/MXSRessources.swift
@@ -0,0 +1,23 @@
+//
+//  Ressources.swift
+//  MeetXSweat
+//
+//  Created by Mohamed BOUMANSOUR on 9/21/16.
+//  Copyright © 2016 Mohamed BOUMANSOUR. All rights reserved.
+//
+
+import Foundation
+
+
+/// A list of different kind of ressources.
+enum Ressources {
+    
+    /**
+     *  A struct of constants about the images.
+     */
+    struct Images {
+        
+        static let profilePlaceHolder    = "Profile_placeholder"
+        static let ProfessionalProfile   = "ProfessionalProfile"
+    }
+}
